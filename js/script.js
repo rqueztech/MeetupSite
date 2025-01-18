d03884cd518657a1ae01f3798649a57ce5284452
rqueztech <rqueztech@noreply.com>
2025-01-18 15:32:07 +0900

    Create lists using Javascript.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 //window.alert("This is a scam website."); Fixed the scam!
+
+document.addEventListener("DOMContentLoaded", function() {
+	const funMeetingNotes = ["Tech companies want to start nuclear plants","Sakana AI - Pretty cool stuff","Nvidia GPU - AI is through the roof (new GPU came out)Tech companies"];
+	const mainContainer = document.getElementById("main-container");
+	const meetingNotes = document.createElement("ul");
+	funMeetingNotes.forEach(note => {
+		const currentList = document.createElement("li");
+		currentList.textContent = (note);
+		meetingNotes.append(currentList);
+	});
+	
+	mainContainer.append(meetingNotes);
+});
